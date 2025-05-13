76255f436d6c33e9e14e0c5ee950b5e615f3ce9e
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-13 16:09:07 +0700

    Prosedur cetak

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -29,3 +29,9 @@ mahasiswa :: mahasiswa (int iNim , string iNama){
     nim = iNim; // Definisi NIM dan NAMA
     nama = iNama;
 }
+
+void mahasiswa::cetak(){
+    
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
