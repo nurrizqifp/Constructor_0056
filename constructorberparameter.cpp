b7e7f9e0bd6df96a89ce0ac9bf848f485df8cc7d
nurrizqifp <nurrizqifebriansyah@gmail.com>
2025-05-13 16:00:50 +0700

    Definisi constructor dengan parameter

--- constructorberparameter.cpp
+++ constructorberparameter.cpp
@@ -7,3 +7,11 @@ class mahasiswa
     public:
     mahasiswa(int nim, string nama); // Langsung Deklarasi Parameter
 };
+
+// Definisi constructor dengan parameter
+mahasiswa::mahasiswa(int nim, string nama)
+{
+cout << "Constructor Terpanggil" << endl;
+cout << "NIM : " << nim << endl;
+cout << "Nama : " << nama << endl;
+}
